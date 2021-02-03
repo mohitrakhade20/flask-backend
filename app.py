095239452f6b20c167c29252374e058b015b8908
Mohit <mohitrakhade20@gmail.com>
2021-02-03 13:33:08 +0530

    flask-restful added

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from database.db import initialize_db
-from resource.movie import movies
+from flask_restful import Api
+from resources.routes import initialize_routes
 
 app = Flask(__name__)
+api = Api(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb+srv://mohit2:mohit2@cluster0.aocc5.mongodb.net/rest-flask?retryWrites=true&w=majority'
 }
 
 initialize_db(app)
-app.register_blueprint(movies)
+initialize_routes(api)
 
 app.run()
