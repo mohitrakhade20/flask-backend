7640a04b409fa896bc847f820aa1980d4c1740df
Mohit <mohitrakhade20@gmail.com>
2021-02-03 15:20:24 +0530

    added Authorization

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
 
 app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
 api = Api(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb+srv://mohit2:mohit2@cluster0.aocc5.mongodb.net/rest-flask?retryWrites=true&w=majority'
@@ -12,5 +17,6 @@ app.config['MONGODB_SETTINGS'] = {
 
 initialize_db(app)
 initialize_routes(api)
+
 
 app.run()
