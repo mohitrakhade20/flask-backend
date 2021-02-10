e47161ef3afcf16466c2eb2c27efe742686c6862
Mohit <mohitrakhade20@gmail.com>
2021-02-10 16:12:29 +0530

    error handling

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
+from resources.errors import errors
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
-api = Api(app)
+
+api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
@@ -17,6 +20,5 @@ app.config['MONGODB_SETTINGS'] = {
 
 initialize_db(app)
 initialize_routes(api)
-
 
 app.run()
